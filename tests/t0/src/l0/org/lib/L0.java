d9b0ee8ef69d95dacd950338612563b38e8af8c8
Adam Ohlsson <karladamohlsson@gmail.com>
2019-10-01 17:38:58 +0000

    Update testcase to include inline method opportunities.

--- tests/t0/src/l0/org/lib/L0.java
+++ tests/t0/src/l0/org/lib/L0.java
@@ -1,5 +1,41 @@
 package org.lib;
 
 public class L0 {
-    public void f() {}
+
+    public void f() {
+        int x = g();
+
+        /* The purpose of this print is to make sure framework does not crash
+           when it encounters method invocations for which the binding can not
+           be resolved. */
+        System.out.println("g() = " + x);
+    }
+
+    private int g() {
+        int sum = 0;
+        for (int i = 0; i < 10; ++i) {
+            sum += i;
+        }
+        return sum;
+    }
+
+    public int h() {
+        for (int i = g(); i > 10; --i) {
+            System.out.println("" + i);
+        }
+        return 5;
+    }
+
+    public int e() {
+        int i = 0;
+        for (i = g(); i > 0; --i) {
+            System.out.println("" + g());
+        }
+        return 0;
+    }
+
+    public L0 newL0() {
+        return new L0();
+    }
+
 }
